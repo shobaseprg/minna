229267ff478eb3a44dfd9be94f85c548dbe04b8c
shobaseprg <pgymlsho19128@gmail.com>
2020-12-26 17:06:36 +0900

    mastar branch create

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,22 +1,22 @@
 
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-function auth(req, res, next) {
-  let token = (req.session && req.session.token) ? req.session.token : null;
+// function auth(req, res, next) {
+//   let token = (req.session && req.session.token) ? req.session.token : null;
 
-  if (!token) { //トークンがなければ
-    res.status(401);
-    res.write(JSON.stringify({ status: false, result: 'No token provided.' }, 2, null));
-    res.end();
-  } else {
-    jwt.verify(token, 'secretKey', function (err) {//トークンが正しいかチェック
-      if (err) {
-        res.status(401);
-        res.write(JSON.stringify({ status: false, result: 'Invalid token.' }, 2, null));
-      } else {
-        next();
-      }
-    });
-  }
-}
-module.exports = auth;
+//   if (!token) { //トークンがなければ
+//     res.status(401);
+//     res.write(JSON.stringify({ status: false, result: 'No token provided.' }, 2, null));
+//     res.end();
+//   } else {
+//     jwt.verify(token, 'secretKey', function (err) {//トークンが正しいかチェック
+//       if (err) {
+//         res.status(401);
+//         res.write(JSON.stringify({ status: false, result: 'Invalid token.' }, 2, null));
+//       } else {
+//         next();
+//       }
+//     });
+//   }
+// }
+// module.exports = auth;
